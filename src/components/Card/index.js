04e772ccc686d6307fe25241485b298d1057ce4d
michel-lp1 <michel.lp@outlook.com>
2023-02-07 21:05:51 -0300

    favoritar

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,19 @@
+import { useFavoritoContext } from 'contextos/Favoritos';
 import styles from './Card.module.css';
-import iconeFavoritar from './favoritar.png';
+import iconeFavoritar from '.favoritar.png';
 
 function Card({ id, titulo, capa }) {
+    const { favorito, adicionarFavorito } = useFavoritoContext();
     return (
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
             <img src={iconeFavoritar}
                 alt="Favoritar filme"
-                className={styles.favoritar} />
+                className={styles.favorito}
+                onClick={() => {
+                    adicionarFavorito({ id, titulo, capa })
+                }} />
         </div>
 
     )
